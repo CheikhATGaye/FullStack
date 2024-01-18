b27146e9d3dae1b7a9497f1bb1d52974d92c9ae9
Cheikh Ahmed Tidiane Gaye <cheikhatgaye235@gmail.com>
2024-01-18 23:36:00 +0100

    Button and display component

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -23,15 +23,23 @@ const Total = (props) => {
     </div>
   )
 }
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  
-  const setToZero = () => setCounter(0)
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -49,19 +57,29 @@ const App = () => {
       }
     ]
   }
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)  
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
   <div>
    <div>
-   <div>{counter}</div>
-   <button onClick={increaseByOne}>
-    plus
-   </button>
-   <button onClick={setToZero}> 
-    zero
-   </button>
+   <Display counter={counter}/>
+   <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />   
   </div>
-   <div>
+  <div>
     <Header course = {course.name}/>
     <Content name = {course.parts[0].name} number = {course.parts[0].exercises}/>
     <Content name = {course.parts[1].name} number = {course.parts[1].exercises}/>

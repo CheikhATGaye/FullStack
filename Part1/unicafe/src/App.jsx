5e46fb47a7dcbcc6caa80de8cab21a1608757d7d
Cheikh Ahmed Tidiane Gaye <cheikhatgaye235@gmail.com>
2024-01-21 14:55:10 +0100

    step5-4

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 
-const Text = (props) =>{
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
+const Title = (props) => <h1>{props.text}</h1>
+const Statistic = (props) => <h1>{props.statistic}</h1>
+const StatisticLine = (props) =>{
+  return(<div>
+    {props.text} {props.value}
+  </div>)
+}
+const Button = (props) => {
+  return(
+  <button onClick={props.onClick}>
+    {props.name}
+  </button>
   )
 }
 const App = () => {
@@ -12,29 +19,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const leftButtonClick = () => setGood(good + 1)
+  const leftButtonClick = () => {
+    const updateGood = good + 1
+    setGood(updateGood)
+    setTotal(updateGood + neutral + bad)
+  }
 
-  const middleButtonClick = () => setNeutral(neutral + 1)
+  const middleButtonClick = () => {
+    const updateNeutral = neutral +1
+    setNeutral(updateNeutral)
+    setTotal(good + updateNeutral + bad)
+  }
 
-  const rightButtonClick = () => setBad(bad + 1)
+  const rightButtonClick = () => {
+    const updateBad = bad +1
+    setBad(updateBad)
+    setTotal(good + neutral+ updateBad)
+  }
+
+    let average = (good - bad)/total;
+    let positive = good/total;
   
-
-  return (
+    return (
     <div>
-    <Text name = {'give feedback'}/>
+    <div><Title text = {'give feedback'}/></div>
     <div>
-      <button onClick={leftButtonClick}>good
-      </button> 
-      <button onClick={middleButtonClick}>neutral
-      </button>
-      <button onClick={rightButtonClick}>bad
-      </button>
+      <Button onClick={leftButtonClick} name={'good'}/> 
+      <Button onClick={middleButtonClick} name={'neutral'}/>
+      <Button onClick={rightButtonClick} name={'bad'}/>
     </div>
-      <Text name = {'statistic'}/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+    <div>
+    <Statistic statistic = {'statistic'}/>
+    </div>
+    <div>
+      <StatisticLine text = {'good'} value = {good}/>
+      <StatisticLine text = {'neutral'} value = {neutral}/>
+      <StatisticLine text = {'bad'} value = {bad}/>
+      <StatisticLine text = {'all'} value = {total}/>
+      <StatisticLine text = {'average'} value = {average}/>
+      <StatisticLine text = {'positive'} value = {positive}/>
+    </div>
     </div>
   )
 }
